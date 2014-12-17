171b730fd7537a66c6ede7ba567407fcdc10d2af
baugh70 <zakkyzakkba@gmail.com>
2014-12-16 21:18:42 -0500

    Removed unecessary import

--- src/us/legioncraft/inventorygui/example/ExampleMain.java
+++ src/us/legioncraft/inventorygui/example/ExampleMain.java
@@ -2,7 +2,6 @@ package us.legioncraft.inventorygui.example;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-import us.legioncraft.inventorygui.inventory.GUI;
 import us.legioncraft.inventorygui.inventory.InventoryManager;
 
 public class ExampleMain extends JavaPlugin{
