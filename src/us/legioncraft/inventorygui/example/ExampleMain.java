effac4e050651b27f3466f64a7894a7afe176862
baugh70 <zakkyzakkba@gmail.com>
2014-12-16 21:17:52 -0500

    Added a command example

--- src/us/legioncraft/inventorygui/example/ExampleMain.java
+++ src/us/legioncraft/inventorygui/example/ExampleMain.java
@@ -2,6 +2,7 @@ package us.legioncraft.inventorygui.example;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import us.legioncraft.inventorygui.inventory.GUI;
 import us.legioncraft.inventorygui.inventory.InventoryManager;
 
 public class ExampleMain extends JavaPlugin{
